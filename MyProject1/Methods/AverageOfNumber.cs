59122233917eb1a7825c506a299bfca63e40a67b
shivamt9977 <shivam31t@gmail.com>
2022-08-17 17:14:02 +0530

    Methods_demo

---
+++ MyProject1/Methods/AverageOfNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject1.Methods
+{
+    class AverageOfNumber
+    {
+        public double IsAverage(int num)
+        {
+            int sum = 0;
+            int count = 0;
+            while(num!=0)
+            {
+                int r = num % 10;
+                sum = sum + r;
+                num = num / 10;
+                count++;
+            }
+            double avrg = sum / count;
+            return avrg;
+        }
+        static void Main(string[] args)
+        {
+            AverageOfNumber a = new AverageOfNumber();
+            Console.WriteLine("Enter the Number");
+            int n = int.Parse(Console.ReadLine());
+
+           double ans= a.IsAverage(n);
+            Console.WriteLine("Average is "+ans);
+        }
+    }
+}
---
+++ MyProject1/Methods/CheckPrime.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject1.Methods
+{
+    class CheckPrime
+    {
+        public bool IsPrime(int num)
+        {
+            int i = 2;
+            int count = 0;
+            while(i<=num)
+            {
+                if(num%i==0)
+                {
+                    count++;
+                }
+                i++;
+            }
+            if(count==1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CheckPrime p = new CheckPrime();
+            Console.WriteLine("Enter the Number");
+            int num = int.Parse(Console.ReadLine());
+
+           bool IsAprime= p.IsPrime(num);
+            if(IsAprime==true)
+            {
+                Console.WriteLine("Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+        }
+    }
+}
