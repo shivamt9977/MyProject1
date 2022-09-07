fdc2191bf74bdf449d01b4a5b1311c9d1a77f850
shivamt9977 <shivam31t@gmail.com>
2022-09-07 17:27:59 +0530

    7_sep

---
+++ MyProject1/Methods1/CallByRefrence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject1.Methods1
+{
+    class m2
+    {
+        public void m1(ref int a ,ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+    }
+
+    class CallByRefrence
+    {
+        static void Main(string[] args)
+        {
+            int x = 90, y = 100;
+            m2 m = new m2();
+            m.m1(ref x, ref y);
+            Console.WriteLine("X is "+x+" Y is "+y);
+        }
+      
+
+    }
+}
